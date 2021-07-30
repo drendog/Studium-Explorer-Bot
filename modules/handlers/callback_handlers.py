9390498c4a917593baa67017fd1402218a803289
drendog <graziano27dg@gmail.com>
2021-07-30 17:05:44 +0200

    fix: new domains checks fix

--- modules/handlers/callback_handlers.py
+++ modules/handlers/callback_handlers.py
@@ -15,11 +15,13 @@ def course_callback(update: Update, context: CallbackContext) -> int:
 
     Returns:
         int: next state of the conversation
-    """  
+    """
 
     query = update.callback_query
 
     url: str = query.data
+
+    # print(url)
 
     if not url.startswith('http'):
         if context.user_data['base_url'] == None:
@@ -29,23 +31,26 @@ def course_callback(update: Update, context: CallbackContext) -> int:
         else:
             url = context.user_data['last_url'] + url
 
-    if url.find('category=') != -1 or len(url) <= 36:
+    if url.find('category=') != -1 or len(url) <= 36 or (url.find("studiumarchive.unict.it/dokeos/") != -1 and len(url) <= 43):
         studium_out = StudiumScraper.get_cats(url)
     elif url.find('/courses/') != -1:
         if is_file(url):
-            send_file(update=update, context=context, url=url) 
-            return STATE['end']   
+            send_file(update=update, context=context, url=url)
+            return STATE['end']
         studium_out = StudiumScraper.get_files_list(url)
     else:
-        query.edit_message_text(text=f"Cannot get this path. Please /start again.")
+        query.edit_message_text(
+            text=f"Cannot get this path. Please /start again.")
         return STATE['end']
-    
+
     context.user_data['last_studium_out'] = studium_out
     context.user_data['shift_menu_index'] = 0
     context.user_data['last_url'] = url
-    context.user_data['base_url'] = url[:url.find('unict.it') + len('unict.it')]
+    context.user_data['base_url'] = url[:url.find(
+        'unict.it') + len('unict.it')]
 
-    generate_keyboard(update=update.callback_query, context=context, studium_out=studium_out, message_text='Seleziona l\'anno accademico')
+    generate_keyboard(update=update.callback_query, context=context,
+                      studium_out=studium_out, message_text='Seleziona l\'anno accademico')
 
     return STATE['select_department']
 
@@ -58,7 +63,7 @@ def is_file(url: str) -> bool:
 
     Returns:
         bool: return true if is a file
-    """    
+    """
     last_folder_index = url.rfind('/')
     if url.find('.', last_folder_index) != -1:
         return True
@@ -72,7 +77,7 @@ def send_file(update: Update, context: CallbackContext, url: str) -> None:
         update (Update): update event
         context (CallbackContext): context passed by the handler
         url (str): url
-    """    
+    """
     query = update.callback_query
     query.edit_message_text(text=f'[📥]({url}) *Download File*\. If you need another file, /start again\.',
                             parse_mode=ParseMode.MARKDOWN_V2)
@@ -84,9 +89,10 @@ def shift_menu_index_down(update: Update, context: CallbackContext) -> None:
     Args:
         update (Update): update event
         context (CallbackContext): context passed by the handler
-    """    
+    """
     context.user_data['shift_menu_index'] += config_map['max_buttons']
-    generate_keyboard(update=update.callback_query, context=context, studium_out=context.user_data['last_studium_out'], message_text='Seleziona l\'anno accademico')
+    generate_keyboard(update=update.callback_query, context=context,
+                      studium_out=context.user_data['last_studium_out'], message_text='Seleziona l\'anno accademico')
 
 
 def shift_menu_index_up(update: Update, context: CallbackContext) -> None:
@@ -95,6 +101,7 @@ def shift_menu_index_up(update: Update, context: CallbackContext) -> None:
     Args:
         update (Update): update event
         context (CallbackContext): context passed by the handler
-    """ 
+    """
     context.user_data['shift_menu_index'] -= config_map['max_buttons']
-    generate_keyboard(update=update.callback_query, context=context, studium_out=context.user_data['last_studium_out'], message_text='Seleziona l\'anno accademico')
+    generate_keyboard(update=update.callback_query, context=context,
+                      studium_out=context.user_data['last_studium_out'], message_text='Seleziona l\'anno accademico')
